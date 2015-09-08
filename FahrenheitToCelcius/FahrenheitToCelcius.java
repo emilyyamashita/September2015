69d1bee29e8ee0490c59983b943dbafa47f4900b
emilyyamashita <emilyfumie@gmail.com>
2015-09-08 09:23:04 -0700

    Fahrenheit to Celcius
    formula

---
+++ FahrenheitToCelcius/FahrenheitToCelcius.java
@@ -0,0 +1,18 @@
+
+/**
+ * This program will show how to convert temperatures in Fahrenheit to Celcius
+ * 
+ * @author Emily Yamashita
+ * @version v1 09/8/2015
+ */
+public class FahrenheitToCelcius
+{
+    // F: variable for fahrenheit
+    //random assigned value: 1.0 degree Fahrenheit
+    double f=1.0;
+    // C: variable for Celcius
+    double C= (5%9)*(f-32);
+    System.out.print(C);
+}
+    
+    
